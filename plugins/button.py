4e33a41a85b6f0ac6dd0728dc305fe2765f0f0e7
PyroUserBot <129763781+PyroUserBot@users.noreply.github.com>
2023-09-11 22:58:58 +0530

    Update button.py

--- plugins/button.py
+++ plugins/button.py
@@ -10,10 +10,10 @@ def start_button(client):
             ],
         ]
         return buttons
-    if not FORCE_SUB_CHANNEL and FORCE_SUB_GROUP:
+    if not FORCE_SUB_CHANNEL and FORCE_SUB_CHANNEL3:
         buttons = [
             [
-                InlineKeyboardButton(text="Group", url=client.invitelink2),
+                InlineKeyboardButton(text="Group", url=client.invitelink3),
             ],
             [
                 InlineKeyboardButton(text="Donate", callback_data="help"),
@@ -21,10 +21,10 @@ def start_button(client):
             ],
         ]
         return buttons
-    if FORCE_SUB_CHANNEL and not FORCE_SUB_GROUP:
+    if FORCE_SUB_CHANNEL and not FORCE_SUB_CHANNEL2:
         buttons = [
             [
-                InlineKeyboardButton(text="Channel", url=client.invitelink),
+                InlineKeyboardButton(text="Channel", url=client.invitelink4),
             ],
             [
                 InlineKeyboardButton(text="Donate", callback_data="help"),
@@ -44,56 +44,13 @@ def start_button(client):
             [InlineKeyboardButton(text="Close", callback_data="close")],
         ]
         return buttons
-    if not FORCE_SUB_CHANNEL2 and not FORCE_SUB_CHANNEL3:
-        buttons = [
-            [
-                InlineKeyboardButton(text="Donate", callback_data="help"),
-                InlineKeyboardButton(text="Close ", callback_data="close"),
-            ],
-        ]
-        return buttons
-    if not FORCE_SUB_CHANNEL2 and FORCE_SUB_CHANNEL3:
-        buttons = [
-            [
-                InlineKeyboardButton(text="Group", url=client.invitelink4),
-            ],
-            [
-                InlineKeyboardButton(text="Donate", callback_data="help"),
-                InlineKeyboardButton(text="Close", callback_data="close"),
-            ],
-        ]
-        return buttons
-    if FORCE_SUB_CHANNEL2 and not FORCE_SUB_CHANNEL3:
-        buttons = [
-            [
-                InlineKeyboardButton(text="Channel", url=client.invitelink3),
-            ],
-            [
-                InlineKeyboardButton(text="Donate", callback_data="help"),
-                InlineKeyboardButton(text="Close", callback_data="close"),
-            ],
-        ]
-        return buttons
-    if FORCE_SUB_CHANNEL2 and FORCE_SUB_CHANNEL3:
-        buttons = [
-            [
-                InlineKeyboardButton(text="Donate", callback_data="help"),
-            ],
-            [
-                InlineKeyboardButton(text="Channel 3", url=client.invitelink3),
-                InlineKeyboardButton(text="Channel 4", url=client.invitelink4),
-            ],
-            [InlineKeyboardButton(text="Close", callback_data="close")],
-        ]
-        return buttons
-
 
 
 def fsub_button(client, message):
     if not FORCE_SUB_CHANNEL and FORCE_SUB_GROUP:
         buttons = [
             [
-                InlineKeyboardButton(text="Join Channel 3", url=client.invitelink3),
+                InlineKeyboardButton(text="Join Channel 2", url=client.invitelink2),
             ],
         ]
         try:
@@ -110,6 +67,41 @@ def fsub_button(client, message):
         return buttons
     if FORCE_SUB_CHANNEL and not FORCE_SUB_GROUP:
         buttons = [
+            [
+                InlineKeyboardButton(text="Channel 1", url=client.invitelink),
+            ],
+        ]
+        try:
+            buttons.append(
+                [
+                    InlineKeyboardButton(
+                        text="Restart",
+                        url=f"https://t.me/{client.username}?start={message.command[1]}",
+                    )
+                ]
+            )
+        except IndexError:
+            pass
+        return buttons
+    if FORCE_SUB_CHANNEL and FORCE_SUB_GROUP:
+        buttons = [
+            [
+                InlineKeyboardButton(text="Channel 1", url=client.invitelink),
+                InlineKeyboardButton(text="Channel 2", url=client.invitelink2),
+            ],
+        ]
+        try:
+            buttons.append(
+                [
+                    InlineKeyboardButton(
+                        text="Restart ",
+                        url=f"https://t.me/{client.username}?start={message.command[1]}",
+                    )
+                ]
+            )
+        except IndexError:
+            pass
+        return buttons
             [
                 InlineKeyboardButton(text="Channel 1", url=client.invitelink),
             ],
