ab4945f59ea1f0fe9f48c873e54875924e941eed
PyroUserBot <129763781+PyroUserBot@users.noreply.github.com>
2023-09-12 01:37:22 +0530

    Update updater.py

--- plugins/updater.py
+++ plugins/updater.py
@@ -1,12 +1,6 @@
-# Credits: @mrismanaziz
-# FROM File-Sharing-Man <https://github.com/mrismanaziz/File-Sharing-Man/>
-# t.me/SharingUserbot & t.me/Lunatic0de
-
 import os
 import sys
 from os import environ, execle, system
-
-from bot import Bot
 from git import Repo
 from git.exc import InvalidGitRepositoryError
 from pyrogram import Client, filters
@@ -17,23 +11,21 @@ from config import ADMINS, LOGGER
 UPSTREAM_REPO = "https://github.com/PyroUserBot/MULTIFSUB"
 
 
-def gen_chlog(repo, diff):
+def generate_changelog(repo, diff):
     upstream_repo_url = Repo().remotes[0].config_reader.get("url").replace(".git", "")
-    ac_br = repo.active_branch.name
-    ch_log = ""
-    tldr_log = ""
-    ch = f"<b>updates for <a href={upstream_repo_url}/tree/{ac_br}>[{ac_br}]</a>:</b>"
-    ch_tl = f"updates for {ac_br}:"
-    d_form = "%d/%m/%y || %H:%M"
-    for c in repo.iter_commits(diff):
-        ch_log += (
-            f"\n\n💬 <b>{c.count()}</b> 🗓 <b>[{c.committed_datetime.strftime(d_form)}]</b>\n<b>"
-            f"<a href={upstream_repo_url.rstrip('/')}/commit/{c}>[{c.summary}]</a></b> 👨‍💻 <code>{c.author}</code>"
+    active_branch = repo.active_branch.name
+    changelog = ""
+    tldr_changelog = ""
+    date_format = "%d/%m/%y || %H:%M"
+    for commit in repo.iter_commits(diff):
+        changelog += (
+            f"\n\n💬 <b>{commit.count()}</b> 🗓 <b>[{commit.committed_datetime.strftime(date_format)}]</b>\n<b>"
+            f"<a href={upstream_repo_url}/commit/{commit}>[{commit.summary}]</a></b> 👨‍💻 <code>{commit.author}</code>"
         )
-        tldr_log += f"\n\n💬 {c.count()} 🗓 [{c.committed_datetime.strftime(d_form)}]\n[{c.summary}] 👨‍💻 {c.author}"
-    if ch_log:
-        return str(ch + ch_log), str(ch_tl + tldr_log)
-    return ch_log, tldr_log
+        tldr_changelog += f"\n\n💬 {commit.count()} 🗓 [{commit.committed_datetime.strftime(date_format)}]\n[{commit.summary}] 👨‍💻 {commit.author}"
+    if changelog:
+        return str(f"<b>Updates for <a href={upstream_repo_url}/tree/{active_branch}>[{active_branch}]</a>:</b>" + changelog), str(f"Updates for {active_branch}:" + tldr_changelog)
+    return changelog, tldr_changelog
 
 
 def updater():
@@ -46,24 +38,22 @@ def updater():
         repo.create_head("master", origin.refs.master)
         repo.heads.master.set_tracking_branch(origin.refs.master)
         repo.heads.master.checkout(True)
-    ac_br = repo.active_branch.name
+    active_branch = repo.active_branch.name
     if "upstream" in repo.remotes:
-        ups_rem = repo.remote("upstream")
+        ups_remote = repo.remote("upstream")
     else:
-        ups_rem = repo.create_remote("upstream", UPSTREAM_REPO)
-    ups_rem.fetch(ac_br)
-    changelog, tl_chnglog = gen_chlog(repo, f"HEAD..upstream/{ac_br}")
+        ups_remote = repo.create_remote("upstream", UPSTREAM_REPO)
+    ups_remote.fetch(active_branch)
+    changelog, tl_changelog = generate_changelog(repo, f"HEAD..upstream/{active_branch}")
     return bool(changelog)
-
 
 
 @Bot.on_message(filters.command("update") & filters.user(ADMINS))
 async def update_bot(_, message: Message):
-    message.chat.id
-    msg = await message.reply_text("Checking updates...")
-    update_avail = updater()
-    if update_avail:
-        await msg.edit("✅ Update finished !")
+    msg = await message.reply_text("Checking for updates...")
+    update_available = updater()
+    if update_available:
+        await msg.edit("✅ Update finished!")
         system("git pull -f && pip3 install --no-cache-dir -r requirements.txt")
         execle(sys.executable, sys.executable, "main.py", environ)
         return
@@ -77,9 +67,9 @@ async def update_bot(_, message: Message):
 async def restart_bot(_, message: Message):
     try:
         msg = await message.reply_text("`Restarting bot...`")
-        LOGGER(__name__).info("BOT SERVER RESTARTED !!")
+        LOGGER(__name__).info("BOT SERVER RESTARTED!")
     except BaseException as err:
         LOGGER(__name__).info(f"{err}")
         return
-    await msg.edit_text("✅ Bot has restarted !\n\n")
+    await msg.edit_text("✅ Bot has restarted!\n\n")
     os.system(f"kill -9 {os.getpid()} && bash start")
